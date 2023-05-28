cb56a829d25bacfb55618f5a40cd735568dd7a04
jeongyeon9254 <jeongyeon9254@naver.com>
2023-05-28 16:32:45 +0900

    feat : 시각장애인 클라리언트 초기

---
+++ pages/offer/index.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+import styled from "styled-components";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <>
+      <div>오퍼</div>
+    </>
+  );
+}
