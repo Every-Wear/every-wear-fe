9856f8e0e2180d9e9cf6d3a21258f8cd8bbeb359
DevSORA <sodaoov@gmail.com>
2023-06-01 21:24:32 +0900

    fix: pinga -> ping 오타 수정

--- api/modules/user.ts
+++ api/modules/user.ts
@@ -3,7 +3,7 @@ import { instance } from "@/api/instance";
 export const get_ping = async () => {
   const response = await instance({
     method: "get",
-    url: `pinga`,
+    url: `ping`,
   });
 
   return response;
