a7e1bc5113c0af1b917dcfc38893a61b0728265a
DevSORA <sodaoov@gmail.com>
2023-06-01 21:27:19 +0900

    feat: matching detail 정보 가져오는 api추가

--- api/modules/matching.ts
+++ api/modules/matching.ts
@@ -1,4 +1,5 @@
 import { instance } from "@/api/instance";
+import { MatchingStatusType } from "@/types/types";
 
 const MATCHING_STATUS_TYPE = {
   매칭대기중: "매칭대기중",
@@ -7,9 +8,8 @@ const MATCHING_STATUS_TYPE = {
   진행중: "진행중",
   진행완료: "진행완료",
 } as const;
-export type MatchingStatusType = keyof typeof MATCHING_STATUS_TYPE;
 
-export const get_matchings = async (matchingsStatus: MatchingStatusType) => {
+const get_matchings = async (matchingsStatus: MatchingStatusType) => {
   const response = await instance({
     method: "get",
     url: `matchings?status=${matchingsStatus}`,
@@ -17,3 +17,14 @@ export const get_matchings = async (matchingsStatus: MatchingStatusType) => {
 
   return response;
 };
+
+const get_matching_detail = async (uuid: string) => {
+  const response = await instance({
+    method: "get",
+    url: `matching/${uuid}`,
+  });
+
+  return response;
+};
+
+export { MATCHING_STATUS_TYPE, get_matchings, get_matching_detail };
