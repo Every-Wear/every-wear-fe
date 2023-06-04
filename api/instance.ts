c60f7296bc566e4f972f5c54cbff0b6f04a4697e
jeongyeon9254 <jeongyeon9254@naver.com>
2023-06-04 22:45:27 +0900

    feat : 로그인페이지, 쿠키

--- api/instance.ts
+++ api/instance.ts
@@ -1,24 +1,32 @@
-import axios from "axios";
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  InternalAxiosRequestConfig,
+} from "axios";
 
-const instance = axios.create({
+import { getCookie } from "@/utils/cookie";
+
+const instance: AxiosInstance = axios.create({
   baseURL: `http://localhost:3000/api/`,
   timeout: 8000,
   headers: {
     "content-type": "application/json;charset=UTF-8",
     accept: "application/json",
-    Authorization:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsaWVudDEiLCJ1c2VyVHlwZSI6ImNsaWVudCIsImlhdCI6MTY4NTg3NTUzNiwiZXhwIjoxNzE3NDExNTM2LCJpc3MiOiJldmVyeS13ZWFyIn0.AbLiuNmHe6stF2zpPmccy6tc54PMXeEKuRpie3x_NOU",
   },
 });
 
-// axios response interceptors
-instance.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (err) {
-    return err;
-  },
-);
+const onRequest = (
+  config: InternalAxiosRequestConfig,
+): InternalAxiosRequestConfig => {
+  const access_token = getCookie("token");
+  config.headers.Authorization = !!access_token ? `${access_token}` : "";
+  return config;
+};
+
+const onErrorRequest = (err: AxiosError | Error): Promise<AxiosError> => {
+  return Promise.reject(err);
+};
+
+instance.interceptors.request.use(onRequest, onErrorRequest);
 
 export { instance };
