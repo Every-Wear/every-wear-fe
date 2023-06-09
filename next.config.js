ee9a06d2ca4ab60cc5fa6ee9afcec11bf352a294
Nuung <qlgks1@naver.com>
2023-06-10 03:11:41 +0900

    modify: domain issue로 nextconfig update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['every-wear.kro.kr', 'localhost']
+    domains: ["everywear.kro.kr", "localhost", "api-everywear.kro.kr"]
   },
 
 };
