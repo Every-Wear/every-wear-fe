39854a9ec2d8b11d666c889673909827665d3f9a
Nuung <qlgks1@naver.com>
2023-06-14 00:25:27 +0900

    modify: temp

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["everywear.kro.kr", "localhost", "api-everywear.kro.kr"]
-  },
-
+    domains: ["everywear.kro.kr", "localhost", "api-everywear.kro.kr"],
+  }
 };
 
 module.exports = nextConfig;
